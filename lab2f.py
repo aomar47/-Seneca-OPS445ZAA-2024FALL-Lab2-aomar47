70551289eba39f0a295def800c6fe00a161d30d1
azra omar <azraalamaomar@yahoo.com>
2024-09-24 22:54:27 -0400

    completed lab

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Azra Omar
+#Author ID: aomar47
+#Date Created: 2024/09/24
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
