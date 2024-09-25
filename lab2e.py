313452d2ae86c20b7447277be81329d1f35b9572
azra omar <azraalamaomar@yahoo.com>
2024-09-24 22:46:50 -0400

    completed lab

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Azra Omar
+#Author ID: aomar47
+#Date Created: 2024/09/24
+
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
